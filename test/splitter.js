835499aa9c0112d9b60f16c3af411ee13a4cc668
smikes <smikes@cubane.com>
2014-11-11 16:27:53 -0700

    early tests
    Former-commit-id: 869550400db868f20e7d82ca4935694ae7527863

--- test/splitter.js
+++ test/splitter.js
@@ -2,10 +2,16 @@
 /*globals describe, it */
 
 var assert = require("assert"),
-    splitter = require("../lib/splitter");
+    Splitter = require("../lib/splitter");
 
 describe("json splitter", function () {
     it("runs a test", function () {
         assert.equal(1 + 2, 3);
+    });
+
+    it("creates a splitter", function () {
+        var s = new Splitter();
+
+        assert.ok(s);
     });
 });
