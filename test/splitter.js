b34ce6c9b6c88d539ac55ed7589077c434ad9a69
smikes <smikes@cubane.com>
2014-11-11 16:19:42 -0700

    basic quality checks

---
+++ test/splitter.js
@@ -0,0 +1,11 @@
+"use strict";
+/*globals describe, it */
+
+var assert = require("assert"),
+    splitter = require("../lib/splitter");
+
+describe("json splitter", function () {
+    it("runs a test", function () {
+        assert.equal(1 + 2, 3);
+    });
+});
