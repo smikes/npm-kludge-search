0d49c8166ee9c12aeed61a99c773654c08305323
smikes <smikes@cubane.com>
2014-11-11 18:32:21 -0700

    add more tests

--- test/splitter.js
+++ test/splitter.js
@@ -19,11 +19,19 @@ describe("json splitter", function () {
         var s = new Splitter();
 
         s.on("data", function (object) {
+            if (object.name === "_updated") {
+                assert.equal(object.value, 1415071799733);
+            }
             if (object.name === "0") {
-                done();
+                assert.deepEqual(object.value, {});
             }
         });
 
+        s.on("end", function () {
+            done();
+        });
+
         s.write('{"_updated":1415071799733,"0":{}}"');
+        s.end();
     });
 });
