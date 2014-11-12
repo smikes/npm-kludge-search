09cecb7e83ce65fa16ec23f718c89eae9b29267a
smikes <smikes@cubane.com>
2014-11-11 17:29:04 -0700

    add some tests

--- test/splitter.js
+++ test/splitter.js
@@ -14,4 +14,16 @@ describe("json splitter", function () {
 
         assert.ok(s);
     });
+
+    it("splits json", function (done) {
+        var s = new Splitter();
+
+        s.on("data", function (object) {
+            if (object.name === "0") {
+                done();
+            }
+        });
+
+        s.write('{"_updated":1415071799733,"0":{}}"');
+    });
 });
