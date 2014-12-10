0f56cfc68ad51e14b4afafa74048acbc0dc88a58
smikes <smikes@cubane.com>
2014-12-10 07:55:37 -0800

    faster unit tests by reusing handle

--- test/test-populateDb.js
+++ test/test-populateDb.js
@@ -1,29 +1,48 @@
 "use strict";
-/*global describe, it*/
+/*global describe, it, afterEach*/
 
 var assert = require('assert'),
     populateDb = require('../lib/populateDb'),
-    makeDb = require('../lib/makeDb'),
+    getDb = require('../lib/getDb'),
     testDB = "./test.sqlite";
 
 describe('populate db', function () {
+    beforeEach(function (done) {
+        getDb(testDB, function (err, db) {
+            db.run("DELETE FROM package;");
+            done();
+        });
+    });
+
     it('can add a record', function (done) {
-        makeDb(testDB, function (err, db) {
+        getDb(testDB, function (err, db) {
             // wrap db
             db = populateDb(db);
 
             db.addPackage({
-                name: 'package',
-                description: 'blah blah foobar',
-                author: 'Near Perfect Memory',
-                date: '2014-04-01T14:21:13',
-                keywords: 'test, sample',
-                url: 'https://github.org/nobody/package/'
-            });
+                    name: 'package',
+                    description: 'blah blah foobar',
+                    author: 'Near Perfect Memory',
+                    date: '2014-04-01T14:21:13',
+                    keywords: 'test, sample',
+                    url: 'https://github.org/nobody/package/'
+                });
 
-            assert(db.findByName('package'))
+                db.findByName('package', function (err, row) {
+                    assert.equal(null, err);
 
-            done();
+                    assert.equal(row.name, 'package');
+                }, done);
+        });
+
+    });
+
+    it('misses missing records', function (done) {
+        getDb(testDB, function (err, db) {
+            // wrap db
+            db = populateDb(db);
+
+            db.findByName('missing', done, done);
         });
     });
 });
