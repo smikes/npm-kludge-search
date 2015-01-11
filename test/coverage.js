8b978896fcb53399db024c06eca45e9ce36b7d2a
smikes <smikes@cubane.com>
2015-01-11 16:00:33 -0700

    remove fts module

--- test/coverage.js
+++ test/coverage.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var fts = require('../lib/fts');
 var populateDb = require('../lib/populateDb');
 var getDb = require('../lib/getDb');
 
@@ -12,36 +11,6 @@ exports.lab = lab;
 var describe = lab.describe;
 var it = lab.it;
 var expect = Code.expect;
-
-describe('angleize', function () {
-    it('puts angles around strings', function (done) {
-        expect(fts.angleize("foo")).to.equal("<foo>");
-        done();
-    });
-
-    it("doesn't barf on undefined", function (done) {
-        expect(fts.angleize()).to.equal();
-        done();
-    });
-});
-
-describe('trimit', function () {
-    it('trims strings', function (done) {
-        expect(fts.trimit("foo")).to.equal("foo");
-        expect(fts.trimit(" foo\t")).to.equal("foo");
-        done();
-    });
-
-    it("doesn't barf on undefined", function (done) {
-        expect(fts.trimit()).to.equal();
-        done();
-    });
-
-    it("returns 'undefined' for numbers", function (done) {
-        expect(fts.trimit(1)).to.equal();
-        done();
-    });
-});
 
 describe('fixTime', function () {
     it('processes time.modified', function (done) {
